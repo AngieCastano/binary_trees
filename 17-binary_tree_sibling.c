c3423290b4ea12145d585f7c893dceef5527ced6
AngieCastano1634 <1634@holbertonschool.com>
2020-07-21 14:35:00 -0500

    [FIX][betty]

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	if (node->parent->right && node->parent->right != node)
 		return (node->parent->right);
-        else if (node->parent->left && node->parent->left != node)
+	else if (node->parent->left && node->parent->left != node)
 		return (node->parent->left);
 	else
 		return (NULL);
