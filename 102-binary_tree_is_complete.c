72209e8da786038d00c4b4e4c773d506af010da5
AngieCastano1634 <1634@holbertonschool.com>
2020-07-22 17:47:59 -0500

    [It crashes some times, but its humble work][to the God]

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,38 +1,84 @@
 #include "binary_trees.h"
 #include <stdio.h>
 /**
- * binary_tree_is_complete_plus - auxiliar function for binary_tree_is_complete
- * @tree: tree to check
- * Return: 1 if is complete, else 0
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: height or 0 if tree is NULL
  */
-int binary_tree_is_complete_plus(const binary_tree_t *tree)
-{
-	int ret_left = 2, ret_right = 1;
 
-	if (tree)
-	{
-		if (tree->right && !tree->left)
-			return (0);
-		if (tree->left && !tree->right)
-			return (2);
-		ret_left = binary_tree_is_complete_plus(tree->left);
-		ret_right = binary_tree_is_complete_plus(tree->right);
-		return (ret_left * ret_right);
-	}
-	return (1);
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	unsigned int h_left = 0, h_right = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	h_left = binary_tree_height(tree->left);
+	h_left++;
+	h_right =  binary_tree_height(tree->right);
+	h_right++;
+	if (h_left > h_right)
+		return (h_left);
+	else
+		return (h_right);
 }
+
 /**
- * binary_tree_is_complete -  checks if a binary tree is complete
- * @tree: tree to check
- * Return: 1 if is complete, else 0
+ * printer - prints left and right branches of a root
+ * @tree: pointer to the root of tree to print
+ * @current_l: level of root to print
+ * @num_rec: just another variable
+ * Return: Nothing
  */
+
+int printer(const binary_tree_t *tree, int current_l, int *num_rec)
+{
+	int ret_left = 1, ret_right = 1;
+
+	if (!tree)
+		return (1);
+	if (current_l == 0)
+	{
+		++num_rec;
+		if (tree->parent)
+			if (tree->parent->left == tree && !tree->parent->right)
+			{
+				return (2);
+			}
+		if (tree->right && !tree->left)
+			return (3);
+	}
+	if (current_l > 0)
+	{
+		ret_left = printer(tree->left, current_l - 1, num_rec);
+		ret_right = printer(tree->right, current_l - 1, num_rec);
+	}
+	return (ret_left * ret_right);
+}
+
+/**
+ * binary_tree_is_complete - checks if a binary tree is complete
+ * @tree: pointer to the root of tree to print
+ * Return: Nothing
+ */
+
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	int ret_plus = 0;
+	int h = 0, it = 0, ret_printer = 0, uno = 1;
+	int *num_rec = &uno;
 
-	ret_plus = binary_tree_is_complete_plus(tree);
-	if (ret_plus == 0 || ret_plus > 2)
+	if (!tree)
 		return (0);
-	else
-		return (1);
+
+	h = binary_tree_height(tree);
+
+	while (it <= h)
+	{
+		ret_printer = printer(tree, it, num_rec);
+		if (ret_printer > 2)
+			return (0);
+		it++;
+	}
+	return (1);
 }
