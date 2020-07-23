9bfcbc5ef0aef6a49d4fa956f082ed8eb0358ab4
ChristianCampos-55 <1566@holbertonschool.com>
2020-07-23 00:23:30 +0000

    [FIX] Adams adopting cains not of their own

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stdio.h>
 /**
  * checker - checks the whole tree searching a special node
  * @root: root tree
@@ -20,6 +21,23 @@ int checker(const binary_tree_t *root, const binary_tree_t *node_to_check)
 }
 
 /**
+ * checker_parent - checks for the Adam of a specific Cain
+ * @cain: for which Adan will be checked
+ * Return: Adam
+ */
+
+int checker_parent(const binary_tree_t *cain)
+{
+	while (cain->parent)
+	{
+		cain = cain->parent;
+	}
+	return (cain->n);
+
+}
+
+
+/**
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
  * @first: pointer to the first node
  * @second: pointer to the second node
@@ -29,26 +47,27 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 				     const binary_tree_t *second)
 {
 	binary_tree_t *traversal = NULL;
-	int check = 1;
+	int check = 1, adam_first = 0, adam_second = 0;
 
 	if (!first || !second)
 		return (NULL);
 
 	if (first == second)
 		return ((binary_tree_t *)first);
-
+	adam_first = checker_parent(first);
+	adam_second = checker_parent(second);
+	if (adam_first != adam_second)
+		return (NULL);
 	if (first->left == second || first->right == second)
 		return ((binary_tree_t *) first);
 	if (second->left == first || second->right == first)
 		return ((binary_tree_t *) second);
-
 	if (first->left)
 		traversal = (binary_tree_t *)first->left;
 	else if (first->right)
 		traversal = (binary_tree_t *)first->right;
 	else
 		traversal = (binary_tree_t *)first;
-
 	while (check != 0)
 	{
 		if (traversal->parent)
